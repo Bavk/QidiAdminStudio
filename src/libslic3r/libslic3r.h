e12379d85c227e13bd20e3c1eaa832b830adacb2
Bavk <Bavk@users.noreply.github.com>
2026-09-22 04:51:03 +0300

    Brand integrated G-code preview as Qidi Admin Studio

--- src/libslic3r/libslic3r.h
+++ src/libslic3r/libslic3r.h
@@ -3,9 +3,9 @@
 
 #include "libslic3r_version.h"
 #define SLIC3R_APP_FULL_NAME "Qidi Admin Studio"
-#define GCODEVIEWER_APP_NAME "OrcaSlicer G-code Viewer"
-#define GCODEVIEWER_APP_KEY  "OrcaSlicerGcodeViewer"
-#define GCODEVIEWER_BUILD_ID std::string("OrcaSlicer G-code Viewer-") + std::string(SLIC3R_VERSION) + std::string("-RC")
+#define GCODEVIEWER_APP_NAME "Qidi Admin Studio Preview"
+#define GCODEVIEWER_APP_KEY  "QidiAdminStudioPreview"
+#define GCODEVIEWER_BUILD_ID std::string("Qidi Admin Studio Preview-") + std::string(SLIC3R_VERSION) + std::string("-RC")
 
 // this needs to be included early for MSVC (listing it in Build.PL is not enough)
 #include <memory>
